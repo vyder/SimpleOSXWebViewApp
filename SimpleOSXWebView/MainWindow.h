651495bbfe749bf18c3065a2cab0259f03b3fc2f
Vidur Murali <vidur.murali@gmail.com>
2015-02-03 19:22:24 +0530

    Create custom NSWindow - MainWindow

---
+++ SimpleOSXWebView/MainWindow.h
@@ -0,0 +1,13 @@
+//
+//  MainWindow.h
+//  SimpleOSXWebView
+//
+//  Created by Vidur Murali on 2/3/15.
+//  Copyright (c) 2015 Vidur Murali. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface MainWindow : NSWindow
+
+@end
---
+++ SimpleOSXWebView/MainWindow.m
@@ -0,0 +1,13 @@
+//
+//  MainWindow.m
+//  SimpleOSXWebView
+//
+//  Created by Vidur Murali on 2/3/15.
+//  Copyright (c) 2015 Vidur Murali. All rights reserved.
+//
+
+#import "MainWindow.h"
+
+@implementation MainWindow
+
+@end
