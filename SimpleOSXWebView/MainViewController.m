9cb8e4b1c5ac062a670324b7fea5710e31d12d27
Vidur Murali <vidur.murali@gmail.com>
2015-02-04 11:02:58 +0530

    Introduces a spinning progress indicator

--- SimpleOSXWebView/MainViewController.m
+++ SimpleOSXWebView/MainViewController.m
@@ -13,6 +13,7 @@
 
 @property NSRect frame;
 @property (strong, nonatomic) WebView *webView;
+@property (strong, nonatomic) NSProgressIndicator *progressSpinner;
 
 @end
 
@@ -33,6 +34,21 @@
     // Create WebView and add it to the parent view
     self.webView = [[WebView alloc] initWithFrame:self.frame];
     [self.view addSubview:self.webView];
+    
+    // Respond to WebFrameLoadDelegate and WebResourceLoadDelegate protocols
+    [self.webView setFrameLoadDelegate:self];
+    [self.webView setResourceLoadDelegate:self];
+    
+    // Create the progress spinner and add it to the parent
+    NSRect spinnerFrame = NSMakeRect(364.0, 270.0, 32.0, 32.0); // TODO: Remove this janky hardcoded positioning
+    self.progressSpinner = [[NSProgressIndicator alloc] initWithFrame:spinnerFrame];
+    [self.progressSpinner setStyle:NSProgressIndicatorSpinningStyle];
+    
+    // NO indicates that the spinner will be hidden when not animating
+    [self.progressSpinner setDisplayedWhenStopped:NO];
+    
+    // Add spinner above webView so it's visible
+    [self.view addSubview:self.progressSpinner positioned:NSWindowAbove relativeTo:self.webView];
 }
 
 - (void)viewDidLoad {
@@ -43,9 +59,6 @@
     [[self.webView mainFrame] loadRequest:[NSURLRequest requestWithURL:url]];
     
     NSLog(@"Should be loading URL [%@] now...", url);
-    
-    // Respond to WebResourceLoadDelegate protocol
-    [self.webView setResourceLoadDelegate:self];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
@@ -54,13 +67,23 @@
     // Update the view, if already loaded.
 }
 
+- (void)webView:(WebView *)sender didStartProvisionalLoadForFrame:(WebFrame *)frame {
+    // Start the spinner when the webView frame starts loading a new page
+    [self.progressSpinner startAnimation:self];
+}
+
+- (void)webView:(WebView *)sender didFinishLoadForFrame:(WebFrame *)frame {
+    // Stop the spinner when the webView frame is done loading.
+    [self.progressSpinner stopAnimation:self];
+}
+
 - (void)webView:(WebView *)sender resource:(id)identifier didFinishLoadingFromDataSource:(WebDataSource *)dataSource {
-    NSLog(@"Page resource should be done loading: %@", [[dataSource request] URL]);
+//    NSLog(@"Page resource should be done loading: %@", [[dataSource request] URL]);
 }
 
 // from InteractionEventListenerProtocol
 - (void)onInteractionEvent:(NSEvent *)theEvent {
-    NSLog(@"Received an interaction event at MainViewController: %@", theEvent );
+//    NSLog(@"Received an interaction event at MainViewController: %@", theEvent );
 }
 
 @end
